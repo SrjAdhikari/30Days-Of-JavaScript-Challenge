1a3efc7e1551c6e5bb86adcb183efb32d0c77fb9
SrjAdhikari <surajadhikari055@gmail.com>
2024-07-16 14:55:04 +0900

    Activity 1: Arithmetic Operations Complete

---
+++ Day2 - Operators/task.js
@@ -0,0 +1,40 @@
+//* **************************************************************
+//* Day 1 : Operators
+//* **************************************************************
+
+
+//* Activity 1: Arithmetic Operations
+
+//* Task 1: Write a program to add two numbers and log the result to the console.
+const firstNum = 10;
+const secondNum = 20;
+const result = firstNum + secondNum;
+console.log(`Result is : ${result}`);               // Output -> Result is : 30
+
+
+//* Task 2: Write a program to subtract two numbers and log the result to the console.
+const operand1 = 50;
+const operand2 = 30;
+const difference = operand1 - operand2;
+console.log(`Difference is : ${difference}`);       // Output -> Difference is : 20
+
+
+//* Task 3: Write a program to multiply two numbers and log the result to the console.
+const firstValue = 11;
+const secondValue = 10;
+const product = firstValue * secondValue;
+console.log(`Product is : ${product}`);             // Output -> Product is : 110
+
+
+//* Task 4: Write a program to divide two numbers and log the result to the console.
+const divisor = 5;
+const dividend = 40;
+const quotient = dividend / divisor;
+console.log(`Quotient is : ${quotient}`);           // Output -> Quotient is : 8
+
+
+//* Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+const divisor1 = 5;
+const dividend1 = 43;
+const remainder = dividend1 % divisor1;
+console.log(`Remainder is : ${remainder}`);         // Output -> Remainder is : 3   
