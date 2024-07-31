cade6aa62d806f9fca8b142487acf7c33b29ff0b
SrjAdhikari <surajadhikari055@gmail.com>
2024-07-31 22:21:26 +0900

    Task-2 Complete

--- Day_13_Modules/task.js
+++ Day_13_Modules/task.js
@@ -16,3 +16,11 @@ const num1 = 10;
 const num2 = 15;
 const result = add(num1, num2);
 console.log(`The sum of ${num1} and ${num2} is: ${result}`);
+
+
+//* Task 2: Create a module that exports an object representing a person with properties and methods. Import and use this module in another script.
+
+// Import the 'person' object from the 'utility.js' module
+import { person } from "./utility.js";
+console.log(`My name is ${person.getFullName()}.`);
+console.log(`My current age is ${person.age}.`);
--- Day_13_Modules/utility.js
+++ Day_13_Modules/utility.js
@@ -7,3 +7,15 @@
 export function add(num1, num2) {
     return num1 + num2;
 }
+
+
+//* Task 2: Create a module that exports an object representing a person with properties and methods. Import and use this module in another script.
+export const person = {
+    firstName : "Suraj",
+    lastName : "Adhikari",
+    age : 25,
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
