7a311efec6c891490ae882ee163a20c4b59a1b11
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-01 09:51:06 +0900

    Task-2 Complete

--- Day_14_Classes/task.js
+++ Day_14_Classes/task.js
@@ -25,3 +25,31 @@ class Person {
 // Create an instance of the Person class
 const person = new Person("Suraj", 26);
 console.log(person.greet());
+
+
+//* Task 2: Add a method to the Person class that updates the age property and logs the updated age.
+
+// Defines a Person object with properties for name and age, and a method to greet others.
+class Person1 {
+    // Constructor to initialize the properties
+    constructor (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    // Method to return a greeting message
+    greet() {
+        return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+    }
+
+    // Method to update the age and log the updated age
+    updateAge(newAge) {
+        this.age = newAge;
+        console.log(`Updated age : ${this.age}`);
+    }
+}
+
+// Create an instance of the Person class
+const person1 = new Person1("Suraj", 25);
+console.log(person1.greet());
+person1.updateAge(26);
