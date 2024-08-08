66c748ce4320bbbb9288b544f01a3a5cd328d041
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-08 10:37:28 +0900

    Day-19 Challenge Complete

--- Day_19_Regular_Expressions/task.js
+++ Day_19_Regular_Expressions/task.js
@@ -245,3 +245,15 @@ if (isValid2) {
 } else {
     console.log('URL is invalid');
 }
+
+
+//* **************************************************************
+//* Achievement:
+//* **************************************************************
+
+//* By the end of these activities, you will:
+//* • Understand and create basic regular expressions.
+//* • Use character classes and quantifiers in regular expressions.
+//* • Implement grouping and capturing in regular expressions.
+//* • Apply assertions and boundaries in regular expressions.
+//* • Use regular expressions for practical applications like validating passwords and URLs.
