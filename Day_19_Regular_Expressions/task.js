0e506b17755e225d92e2f1349d23472a33f5fb8c
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-08 09:35:57 +0900

    Task-3 Complete

--- Day_19_Regular_Expressions/task.js
+++ Day_19_Regular_Expressions/task.js
@@ -40,3 +40,26 @@ const matchDigits = textString.match(regexExp);
 
 // Log the array of matches to the console
 console.log(matchDigits);   // Output -> [ '24', '7', '123' ]
+
+
+//* **************************************************************
+//* Activity 2: Character Classes and Quantifiers
+//* **************************************************************
+
+//* Task 3: Write a regular expression to match all words in a string that start with a capital letter. Log the matches.
+
+// Define the text to search within
+const expression = "This is a 30 Days JavaScript Challenge."
+
+// Define the regular expression to match words starting with a capital letter
+//? \b is a word boundary anchor, ensuring that the match starts at the beginning of a word.
+//? [A-Z] matches any capital letter (A to Z).
+//? \w* matches zero or more word characters (letters, digits, and underscores), completing the rest of the word.
+//? g is the global flag to find all matches in the string.
+const regExp = /\b[A-Z]\w*/g;
+
+// Use the match() method to find all words starting with a capital letter
+const matchWord = expression.match(regExp);
+
+// Log the array of matches to the console
+console.log(matchWord);     // Output -> [ 'This', 'Days', 'JavaScript', 'Challenge' ]
