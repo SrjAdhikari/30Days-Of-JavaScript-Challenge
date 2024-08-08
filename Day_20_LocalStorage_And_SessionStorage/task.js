771206a21f686c18831ffd4f7b4939ac5d18ef6b
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-08 11:15:18 +0900

    Task-1 Complete

---
+++ Day_20_LocalStorage_And_SessionStorage/task.js
@@ -0,0 +1,22 @@
+//* **************************************************************
+//* Day 20 : LocalStorage and SessionStorage
+//* **************************************************************
+
+
+//* **************************************************************
+//* Activity 1: Understanding LocalStorage ✅
+//* **************************************************************
+
+//* Task 1: Write a script to save a string value to localStorage and retrieve it. Log the retrieved value.
+
+// Define the string value to be saved
+const valueToSave = "JavaScript Challenge -> LocalStorage";
+
+// Save the string value to localStorage
+localStorage.setItem("key", valueToSave);
+
+// Retrieve the string value from localStorage
+const retrievedValue = localStorage.getItem("key");
+
+// Log the retrieved value
+console.log(`Retrieved value : ${retrievedValue}`);
